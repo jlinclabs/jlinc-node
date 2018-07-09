2f32d3258248b4d8ada98f2b9fd8f42287b56486
Jared Grippe <jared@deadlyicon.com>
2018-07-09 13:12:43 -0700

    cleaned up expected usage spec

--- test/expectedUsage.spec.js
+++ test/expectedUsage.spec.js
@@ -7,32 +7,27 @@ it('expected usage', function() {
   // done by bob on the B API
 
   const dataCustodian = JLINC.createEntity();
-  // console.log('\ndataCustodian:\n', dataCustodian);
   expect( JLINC.validateDataCustodian({ dataCustodian }) ).to.be.true;
 
   const sisaAgreement = JLINC.createSisaAgreement();
-  // console.log('\nsisaAgreement:\n', sisaAgreement);
   expect( JLINC.validateSisaAgreement({ sisaAgreement }) ).to.be.true;
 
   const sisaOffering = JLINC.createSisaOffering({ sisaAgreement, dataCustodian });
-  // console.log('\nsisaOffering:\n', sisaOffering);
 
   // done by alice on the A API
 
   const { offeredSisa } = sisaOffering;
   expect( JLINC.validateOfferedSisa({ offeredSisa }) ).to.be.true;
-  // console.log('\nofferedSisa:\n', offeredSisa);
 
   const rightsHolder = JLINC.createEntity();
-  // console.log('\nrightsHolder:\n', rightsHolder);
   expect( JLINC.validateRightsHolder({ rightsHolder }) ).to.be.true;
 
   const acceptedSisa = JLINC.acceptSisa({ offeredSisa, rightsHolder });
-  // console.log('\nacceptedSisa:\n', acceptedSisa);
+
+
+  // done by bob on the B API
   expect( JLINC.validateAcceptedSisa({ acceptedSisa }) ).to.be.true;
 
   const expandedAcceptedSisa = JLINC.expandAcceptedSisa({ acceptedSisa });
-  // console.log('\nexpandedAcceptedSisa:\n', expandedAcceptedSisa);
 
-  // done by bob on the B API
 });
