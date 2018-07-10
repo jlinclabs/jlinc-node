423ffca3e071a30a76c17befc8ecf8272f3e2fdc
Jared Grippe <jared@deadlyicon.com>
2018-07-10 14:54:11 -0700

    normalized the mocha test descriptions

--- test/jlinc/index.spec.js
+++ test/jlinc/index.spec.js
@@ -3,24 +3,20 @@
 require('../setup');
 const JLINC = require('../../jlinc');
 
-describe('JLINC', function() {
-
-  describe('.version', function() {
-    it('should be 0.0.1', function(){
-      expect(JLINC.version).to.equal('0.0.1');
-    });
+describe('JLINC.version', function() {
+  it('should be 0.0.1', function(){
+    expect(JLINC.version).to.equal('0.0.1');
   });
+});
 
-  describe('.contextUrl', function() {
-    it('should be "https://context.jlinc.org/v05/jlinc.jsonld"', function(){
-      expect(JLINC.contextUrl).to.equal('https://context.jlinc.org/v05/jlinc.jsonld');
-    });
+describe('JLINC.contextUrl', function() {
+  it('should be "https://context.jlinc.org/v05/jlinc.jsonld"', function(){
+    expect(JLINC.contextUrl).to.equal('https://context.jlinc.org/v05/jlinc.jsonld');
   });
+});
 
-  describe('.defaultAgreementURI', function() {
-    it('should be "https://sisa.jlinc.org/v1/hMwDoQreOrSARtiOG8XqwOs7zolkZRpCLbJ1Dfbv9k4"', function(){
-      expect(JLINC.defaultAgreementURI).to.equal('https://sisa.jlinc.org/v1/hMwDoQreOrSARtiOG8XqwOs7zolkZRpCLbJ1Dfbv9k4');
-    });
+describe('JLINC.defaultAgreementURI', function() {
+  it('should be "https://sisa.jlinc.org/v1/hMwDoQreOrSARtiOG8XqwOs7zolkZRpCLbJ1Dfbv9k4"', function(){
+    expect(JLINC.defaultAgreementURI).to.equal('https://sisa.jlinc.org/v1/hMwDoQreOrSARtiOG8XqwOs7zolkZRpCLbJ1Dfbv9k4');
   });
-
 });
