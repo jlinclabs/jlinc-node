a1c3401bd887ee806da307f3b8c1e6ad0a9b4728
Jared Grippe <jared@deadlyicon.com>
2018-07-10 15:19:21 -0700

    createSignedJwt now explicity uses HS256

--- jlinc/createSignedJwt.js
+++ jlinc/createSignedJwt.js
@@ -3,5 +3,7 @@
 const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = function createSignedJwt({ itemToSign, secret }) {
-  return jsonwebtoken.sign(itemToSign, secret);
+  return jsonwebtoken.sign(itemToSign, secret, {
+    algorithm: 'HS256',
+  });
 };
