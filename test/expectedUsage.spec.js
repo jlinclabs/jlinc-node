2675e173e3b2bcb2e1b4adb8aa5a23e81df004a7
Jared Grippe <jared@deadlyicon.com>
2018-07-12 11:51:26 -0700

    significantly simplified expected usage spec

--- test/expectedUsage.spec.js
+++ test/expectedUsage.spec.js
@@ -5,77 +5,39 @@ const JLINC = require('../jlinc');
 
 it('expected usage', function() {
 
-  /*
-   * done by bob on the B API
-   */
+  // On the B Server
+  const dataCustodian = JLINC.createDataCustodian();
 
-  // bob is a data custodian
-  const dataCustodian = JLINC.createEntity();
+  const dataCustodianId = dataCustodian.publicKey;
 
-  // bob can validate his dataCustodian object anytime
-  expect( JLINC.validateDataCustodian({ dataCustodian }) ).to.be.true;
+  const sisaOffering = JLINC.createSisaOffering({ dataCustodian });
 
-  // bob creates a sisa agreement
-  const sisaAgreement = JLINC.createSisaAgreement();
+  // simulate sending sisa across an HTTP request
+  const copyOfSisaOffering = JSON.parse(JSON.stringify(sisaOffering));
 
-  // bob can validate any sisa agreement he creates
-  expect( JLINC.validateSisaAgreement({ sisaAgreement }) ).to.be.true;
+  // On the A Server
+  const rightsHolder = JLINC.createRightsHolder();
 
-  // bob creates a sisa offering for that sisa agreement
-  const sisaOffering = JLINC.createSisaOffering({ sisaAgreement, dataCustodian });
+  JLINC.validateSisaOffering({
+    sisaOffering: copyOfSisaOffering
+  });
 
-  // bob sends the sisa offering to alice
+  JLINC.verifySisaOfferingIsFromDataCustodian({
+    sisaOffering: copyOfSisaOffering,
+    dataCustodianId,
+  });
 
-  /*
-   * done by alice on the A API
-   */
+  const sisa = JLINC.acceptSisa({
+    sisaOffering: copyOfSisaOffering,
+    rightsHolder,
+  });
 
-  // alice extracts the offered sisa from the sisa offering
-  const { offeredSisa } = sisaOffering;
+  // simulate sending sisa across an HTTP request
+  const copyOfSisa = JSON.parse(JSON.stringify(sisa));
 
-  // alice validates that this offered sisa was signed by the bob she's expecting
-  expect( JLINC.validateOfferedSisa({ offeredSisa, dataCustodian: { publicKey: dataCustodian.publicKey } }) ).to.be.true;
+  // On the B Server
+  JLINC.validateSisa({ sisa: copyOfSisa });
 
-  // an error is thrown when the offered sisa was not signed by the bob alice is expecting
-  expect(() => {
-    JLINC.validateOfferedSisa({ offeredSisa, dataCustodian: { publicKey: JLINC.createEntity().publicKey } });
-  }).to.throw('offeredSisa.dataCustodianId does not match given dataCustodian');
-
-  // alice is a rights holder, she needs these keys for every sisa she signs
-  const rightsHolder = JLINC.createEntity();
-
-  // alice can validate her rightsHolder object anytime
-  expect( JLINC.validateRightsHolder({ rightsHolder }) ).to.be.true;
-
-  // alice accepts the sisa bob offered her
-  const sisa = JLINC.acceptSisa({ offeredSisa, rightsHolder });
-
-  // alice can validate that this sisa was signed by her
-  expect( JLINC.validateSisa({ sisa, rightsHolder }) ).to.be.true;
-
-  // an error is thrown when this sisa is not signed by her
-  expect(() => {
-    JLINC.validateSisa({ sisa, rightsHolder: JLINC.createEntity() });
-  }).to.throw('sisa.acceptedSisa.rightsHolderId does not match given rightsHolder');
-
-  // alice sends bob back the sisa
-
-  /*
-   * done by bob on the B API
-   */
-
-  // bob validates that the returned sisa was originally signed by him
-  expect( JLINC.validateSisa({ sisa, dataCustodian }) ).to.be.true;
-
-  // an error is thrown when the returned sisa is not signed by bob
-  expect(() => {
-    JLINC.validateSisa({ sisa, dataCustodian: JLINC.createEntity() });
-  }).to.throw('sisa.acceptedSisa.offeredSisa.agreementJwt was not signed by the given dataCustodian');
-
-  // bob stores the sisa as well as the expanded sisa
-  const expandedSisa = JLINC.expandSisa({ sisa });
-
-
-
+  JLINC.verifySisaWasOfferedByDataCustodian({ sisa: copyOfSisa, dataCustodian });
 
 });
