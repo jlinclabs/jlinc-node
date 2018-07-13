175d5bbcebc7f6bc6d8781cacbc1c51063cdec3b
Jared Grippe <jared@deadlyicon.com>
2018-07-12 17:42:19 -0700

    added { algorithms: ['HS256'] } to jsonwebtoken.verify

--- jlinc/decodeAndVerifyJwt.js
+++ jlinc/decodeAndVerifyJwt.js
@@ -5,7 +5,7 @@ const jsonwebtoken = require('jsonwebtoken');
 module.exports = function decodeAndVerifyJwt({ jwt, secret }) {
   const { JWTVerificationError } = this;
   try{
-    return jsonwebtoken.verify(jwt, secret);
+    return jsonwebtoken.verify(jwt, secret, { algorithms: ['HS256'] });
   }catch(error){
     if (error.message === 'invalid signature'){
       throw new JWTVerificationError('unable to verify jsonwebtoken');
