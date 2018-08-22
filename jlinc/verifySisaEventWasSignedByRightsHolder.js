f4a96397d1973ecec705fd77cf3d9f50acde7bf9
Jared Grippe <jared@deadlyicon.com>
2018-08-22 12:38:54 -0700

    added tests for JLINC.verifySisaEventWasSignedByRightsHolder

--- jlinc/verifySisaEventWasSignedByRightsHolder.js
+++ jlinc/verifySisaEventWasSignedByRightsHolder.js
@@ -14,7 +14,7 @@ module.exports = function verifySisaEventWasSignedByRightsHolder({ sisaEvent, ri
     });
   }catch(error){
     if (error instanceof InvalidSignatureError)
-      throw new SisaEventVerificationError('sisaOffering is signed by the given rightsHolder');
+      throw new SisaEventVerificationError('sisaEvent was not signed by the given dataCustodian');
     throw error;
   }
 
--- test/jlinc/verifySisaEventWasSignedByRightsHolder.spec.js
+++ test/jlinc/verifySisaEventWasSignedByRightsHolder.spec.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const JLINC = require('../../jlinc');
+const { generateSisaEvent } = require('../helpers');
+
 describe('JLINC.verifySisaEventWasSignedByRightsHolder', function() {
-  it('should have tests');
+  context('when given a sisaEvent', function() {
+    context('that was signed by the given rightsHolder', function() {
+      it('should return true', function(){
+        const { rightsHolder, sisaEvent } = generateSisaEvent();
+        const rightsHolderId = rightsHolder.publicKey;
+        expect(
+          JLINC.verifySisaEventWasSignedByRightsHolder({ sisaEvent, rightsHolderId })
+        ).to.be.true;
+      });
+    });
+    context('that was NOT signed by the given rightsHolder', function() {
+      it('should throw the error "sisaEvent was not signed by the given rightsHolder"', function(){
+        const { sisaEvent } = generateSisaEvent();
+        const rightsHolder = JLINC.createDataCustodian();
+        const rightsHolderId = rightsHolder.publicKey;
+        expect(() => {
+          JLINC.verifySisaEventWasSignedByRightsHolder({ sisaEvent, rightsHolderId });
+        }).to.throw(JLINC.SisaEventVerificationError, 'sisaEvent was not signed by the given dataCustodian');
+      });
+    });
+  });
 });
