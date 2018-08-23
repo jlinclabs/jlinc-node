f4d15d68fc2fee93bdbbf570ccbe54488af5475e
Jared Grippe <jared@deadlyicon.com>
2018-08-23 12:04:26 -0700

    increaded the code coverage for, and simplified, JLINC.validateAcknowledgedSisaEvent

--- jlinc/validateAcknowledgedSisaEvent.js
+++ jlinc/validateAcknowledgedSisaEvent.js
@@ -1,39 +1,22 @@
 'use strict';
 
 module.exports = function validateAcknowledgedSisaEvent({ sisaEvent, acknowledgedSisaEvent }){
-  const { InvalidSignatureError, InvalidAcknowledgedSisaEventError } = this;
+  const { AcknowledgedSisaEventVerificationError, InvalidAcknowledgedSisaEventError } = this;
 
   if (!sisaEvent) throw new Error('sisaEvent is required');
   if (!acknowledgedSisaEvent) throw new Error('acknowledgedSisaEvent is required');
 
   this.validateSisaEvent({ sisaEvent });
-
-  if (acknowledgedSisaEvent.eventJwt !== sisaEvent.eventJwt)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.eventJwt does not match sisaEvent.eventJwt');
-
-  if (acknowledgedSisaEvent.audit.eventType !== sisaEvent.audit.eventType)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.eventType does not match sisaEvent.audit.eventType');
-
-  if (acknowledgedSisaEvent.audit.sisaId !== sisaEvent.audit.sisaId)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.sisaId does not match sisaEvent.audit.sisaId');
+  this.validateSisaEvent({ sisaEvent: acknowledgedSisaEvent });
 
   if (acknowledgedSisaEvent.audit.eventId !== sisaEvent.audit.eventId)
     throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.eventId does not match sisaEvent.audit.eventId');
 
+  if (acknowledgedSisaEvent.audit.eventType !== sisaEvent.audit.eventType)
+    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.eventType does not match sisaEvent.audit.eventType');
+
   if (acknowledgedSisaEvent.audit.timestamp !== sisaEvent.audit.timestamp)
     throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.timestamp does not match sisaEvent.audit.timestamp');
-
-  if (acknowledgedSisaEvent.audit.previousId !== sisaEvent.audit.previousId)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.previousId does not match sisaEvent.audit.previousId');
-
-  if (acknowledgedSisaEvent.audit.rightsHolderSigType !== sisaEvent.audit.rightsHolderSigType)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.rightsHolderSigType does not match sisaEvent.audit.rightsHolderSigType');
-
-  if (acknowledgedSisaEvent.audit.rightsHolderId !== sisaEvent.audit.rightsHolderId)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.rightsHolderId does not match sisaEvent.audit.rightsHolderId');
-
-  if (acknowledgedSisaEvent.audit.rightsHolderSig !== sisaEvent.audit.rightsHolderSig)
-    throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.rightsHolderSig does not match sisaEvent.audit.rightsHolderSig');
 
   if (!acknowledgedSisaEvent.audit.dataCustodianId)
     throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.dataCustodianId is missing');
@@ -45,13 +28,12 @@ module.exports = function validateAcknowledgedSisaEvent({ sisaEvent, acknowledge
     throw new InvalidAcknowledgedSisaEventError('acknowledgedSisaEvent.audit.dataCustodianSig is missing');
 
   try{
-    this.verifySignature({
-      itemSigned: acknowledgedSisaEvent.eventJwt,
-      signature: acknowledgedSisaEvent.audit.dataCustodianSig,
-      publicKey: acknowledgedSisaEvent.audit.dataCustodianId,
+    this.verifyAcknowledgedSisaEventWasSignedByDataCustodian({
+      acknowledgedSisaEvent,
+      dataCustodianId: acknowledgedSisaEvent.audit.dataCustodianId,
     });
   }catch(error){
-    if (error instanceof InvalidSignatureError)
+    if (error instanceof AcknowledgedSisaEventVerificationError)
       throw new InvalidAcknowledgedSisaEventError('sisaEvent.audit.dataCustodianSig is invalid');
     throw error;
   }
--- test/jlinc/validateAcknowledgedSisaEvent.spec.js
+++ test/jlinc/validateAcknowledgedSisaEvent.spec.js
@@ -1,17 +1,12 @@
 'use strict';
 
 const JLINC = require('../../jlinc');
-const { generateSisaEvent } = require('../helpers');
+const { generateAcknowledgedSisaEvent } = require('../helpers');
 
 describe('JLINC.validateAcknowledgedSisaEvent', function() {
 
   before(function() {
-    const { dataCustodian, rightsHolder, sisa, sisaEvent } = generateSisaEvent();
-    const acknowledgedSisaEvent = JLINC.acknowledgeSisaEvent({
-      sisa,
-      dataCustodian,
-      sisaEvent,
-    });
+    const { dataCustodian, rightsHolder, sisa, sisaEvent, acknowledgedSisaEvent } = generateAcknowledgedSisaEvent();
     Object.assign(this, { dataCustodian, rightsHolder, sisa, sisaEvent, acknowledgedSisaEvent });
   });
 
@@ -29,20 +24,127 @@ describe('JLINC.validateAcknowledgedSisaEvent', function() {
     }).to.throw('acknowledgedSisaEvent is required');
   });
 
-  context('when given an invalid acknowledgedSisaEvent', function() {
-    it('should throw the error ""', function(){
+  context('when given an missmatching acknowledgedSisaEvent', function() {
+    it('should throw the error "acknowledgedSisaEvent.eventJwt does not match sisaEvent.eventJwt"', function(){
       const { sisaEvent } = this;
+      const { acknowledgedSisaEvent } = generateAcknowledgedSisaEvent();
       expect(() => {
         JLINC.validateAcknowledgedSisaEvent({
           sisaEvent,
-          acknowledgedSisaEvent: {},
+          acknowledgedSisaEvent,
         });
-      }).to.throw('acknowledgedSisaEvent.eventJwt does not match sisaEvent.eventJwt');
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.eventId does not match sisaEvent.audit.eventId',
+      );
+    });
+  });
+
+  context('when the sisaEvent and the acknowledgedSisaEvent do not match', function() {
+    it('should throw an error', function(){
+      const { sisaEvent, acknowledgedSisaEvent } = this;
+      const { acknowledgedSisaEvent: otherAcknowledgedSisaEvent } = generateAcknowledgedSisaEvent();
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              eventType: 'statusEvent',
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.eventType does not match sisaEvent.audit.eventType',
+      );
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              timestamp: otherAcknowledgedSisaEvent.audit.timestamp+1,
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.timestamp does not match sisaEvent.audit.timestamp',
+      );
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              dataCustodianId: undefined,
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.dataCustodianId is missing',
+      );
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              dataCustodianSigType: undefined,
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.dataCustodianSigType is missing',
+      );
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              dataCustodianSig: undefined,
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'acknowledgedSisaEvent.audit.dataCustodianSig is missing',
+      );
+
+      expect(() => {
+        JLINC.validateAcknowledgedSisaEvent({
+          sisaEvent,
+          acknowledgedSisaEvent: {
+            ...acknowledgedSisaEvent,
+            audit: {
+              ...acknowledgedSisaEvent.audit,
+              dataCustodianId: otherAcknowledgedSisaEvent.audit.dataCustodianId,
+            }
+          },
+        });
+      }).to.throw(
+        JLINC.InvalidAcknowledgedSisaEventError,
+        'sisaEvent.audit.dataCustodianSig is invalid',
+      );
     });
   });
 
   context('when given a acknowledgedSisaEvent that was not acknowledged', function() {
-    it('should throw the error ""', function(){
+    it('should throw the error "acknowledgedSisaEvent.audit.dataCustodianId is missing"', function(){
       const { sisaEvent } = this;
       expect(() => {
         JLINC.validateAcknowledgedSisaEvent({
