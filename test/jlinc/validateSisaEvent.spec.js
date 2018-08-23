24fb94390e749ef791ed91c9b027e7318ee9d75c
Jared Grippe <jared@deadlyicon.com>
2018-08-23 12:17:01 -0700

    incresed the test code coverage for JLINC.validateSisaEvent

--- test/jlinc/validateSisaEvent.spec.js
+++ test/jlinc/validateSisaEvent.spec.js
@@ -6,18 +6,39 @@ const { generateSisaEvent } = require('../helpers');
 describe('JLINC.validateSisaEvent', function() {
 
   before(function() {
-    const { sisaEvent } = generateSisaEvent();
-    this.sisaEvent = sisaEvent;
+    const { sisaEvent, rightsHolder } = generateSisaEvent();
+    Object.assign(this, { sisaEvent, rightsHolder });
   });
 
   context('when given an invalid sisaEvent', function() {
-    it('should return true', function(){
-      const { sisaEvent } = this;
+    it('should throw an error', function(){
+      const { sisaEvent, rightsHolder } = this;
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent is required');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: 45,
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent must be of type object');
+
       expect(() => {
         JLINC.validateSisaEvent({
           sisaEvent: {},
         });
       }).to.throw(JLINC.InsisaEventError, 'sisaEvent must have key "@context"');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': 99,
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent["@context"] is invalid');
+
 
       expect(() => {
         JLINC.validateSisaEvent({
@@ -41,10 +62,32 @@ describe('JLINC.validateSisaEvent', function() {
           sisaEvent: {
             '@context': sisaEvent['@context'],
             eventJwt: sisaEvent.eventJwt,
+            audit: 'boosh',
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit must be of type object');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
             audit: {},
           },
         });
       }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit must have key "eventType"');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: 'shoes',
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.eventType is invalid');
 
       expect(() => {
         JLINC.validateSisaEvent({
@@ -70,6 +113,48 @@ describe('JLINC.validateSisaEvent', function() {
           },
         });
       }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit must have key "createdAt"');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: sisaEvent.audit.eventType,
+              sisaId: sisaEvent.audit.sisaId,
+              createdAt: new Date,
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.createdAt must be of type string');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: sisaEvent.audit.eventType,
+              sisaId: sisaEvent.audit.sisaId,
+              createdAt: 'Sunday the fourth',
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.createdAt must be an ISO Date String');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: sisaEvent.audit.eventType,
+              sisaId: sisaEvent.audit.sisaId,
+              createdAt: new Date(9999999999999).toISOString(),
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.createdAt cannot be in the future');
 
       expect(() => {
         JLINC.validateSisaEvent({
@@ -127,11 +212,46 @@ describe('JLINC.validateSisaEvent', function() {
               eventId: sisaEvent.audit.eventId,
               createdAt: sisaEvent.audit.createdAt,
               previousId: sisaEvent.audit.previousId,
+              rightsHolderSigType: 'xxxx',
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.rightsHolderSigType is invalid');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: sisaEvent.audit.eventType,
+              sisaId: sisaEvent.audit.sisaId,
+              eventId: sisaEvent.audit.eventId,
+              createdAt: sisaEvent.audit.createdAt,
+              previousId: sisaEvent.audit.previousId,
               rightsHolderSigType: sisaEvent.audit.rightsHolderSigType,
             },
           },
         });
       }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit must have key "rightsHolderId"');
+
+      expect(() => {
+        JLINC.validateSisaEvent({
+          sisaEvent: {
+            '@context': sisaEvent['@context'],
+            eventJwt: sisaEvent.eventJwt,
+            audit: {
+              eventType: sisaEvent.audit.eventType,
+              sisaId: sisaEvent.audit.sisaId,
+              eventId: sisaEvent.audit.eventId,
+              createdAt: sisaEvent.audit.createdAt,
+              previousId: sisaEvent.audit.previousId,
+              rightsHolderSigType: sisaEvent.audit.rightsHolderSigType,
+              rightsHolderId: 'cows',
+            },
+          },
+        });
+      }).to.throw(JLINC.InsisaEventError, 'sisaEvent.audit.rightsHolderId must be of length 43');
 
       expect(() => {
         JLINC.validateSisaEvent({
@@ -170,7 +290,6 @@ describe('JLINC.validateSisaEvent', function() {
         });
       }).to.not.throw();
 
-
       expect(() => {
         JLINC.validateSisaEvent({
           sisaEvent: {
@@ -189,7 +308,6 @@ describe('JLINC.validateSisaEvent', function() {
           },
         });
       }).to.throw(JLINC.InvalidSisaEventError, 'sisaEvent.eventJwt is invalid');
-
 
       expect(() => {
         JLINC.validateSisaEvent({
@@ -210,7 +328,6 @@ describe('JLINC.validateSisaEvent', function() {
         });
       }).to.throw(JLINC.InvalidSisaEventError, 'sisaEvent.audit.rightsHolderSig is invalid');
 
-
       expect(() => {
         JLINC.validateSisaEvent({
           sisaEvent: {
@@ -229,6 +346,37 @@ describe('JLINC.validateSisaEvent', function() {
           },
         });
       }).to.throw(JLINC.InvalidSisaEventError, 'sisaEvent.audit.eventId is invalid');
+
+      expect(() => {
+        const eventJwt = 'x.x.x';
+
+        const eventId = JLINC.createHash({
+          itemToHash: `${sisaEvent.audit.sisaId}.${eventJwt}.${sisaEvent.audit.previousId}.${sisaEvent.audit.createdAt}`
+        });
+
+        const rightsHolderSig = JLINC.signItem({
+          itemToSign: eventId,
+          privateKey: rightsHolder.privateKey,
+        });
+
+        const badSisaEvent = {
+          '@context': sisaEvent['@context'],
+          eventJwt,
+          audit: {
+            eventType: sisaEvent.audit.eventType,
+            sisaId: sisaEvent.audit.sisaId,
+            eventId,
+            createdAt: sisaEvent.audit.createdAt,
+            previousId: sisaEvent.audit.previousId,
+            rightsHolderSigType: sisaEvent.audit.rightsHolderSigType,
+            rightsHolderId: sisaEvent.audit.rightsHolderId,
+            rightsHolderSig,
+          },
+        };
+
+        JLINC.validateSisaEvent({ sisaEvent: badSisaEvent });
+      }).to.throw(JLINC.InvalidSisaEventError, 'sisaEvent.eventJwt is invalid');
+
     });
   });
 
