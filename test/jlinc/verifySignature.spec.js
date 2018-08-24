f2749ad755c3166b8e16cf415e60e94ecab6b1aa
Jared Grippe <jared@deadlyicon.com>
2018-08-24 11:04:18 -0700

    improved code coverage for JLINC.verifySignature

--- test/jlinc/verifySignature.spec.js
+++ test/jlinc/verifySignature.spec.js
@@ -4,30 +4,79 @@ const JLINC = require('../../jlinc');
 
 describe('JLINC.verifySignature', function() {
 
-  it('should validate that the given signature is a signature of the give itemSigned and publicKey', function(){
+  beforeEach(function() {
     const rightsHolder = JLINC.createRightsHolder();
-    const itemToSign = 'donkeys need food too';
-
+    const itemSigned = 'donkeys need food too';
     const signature = JLINC.signItem({
-      itemToSign,
+      itemToSign: itemSigned,
       privateKey: rightsHolder.privateKey,
     });
+    const publicKey = rightsHolder.publicKey;
+    const contextUrl = JLINC.contextUrl;
+    Object.assign(this, { itemSigned, signature, publicKey, contextUrl });
+  });
+
+  context('when missing required arguments', function() {
+    it('should throw an error', function(){
+      const { itemSigned, signature, publicKey, contextUrl } = this;
+
+      expect(()=>{
+        JLINC.verifySignature({
+
+        });
+      }).to.throw('itemSigned is required');
+
+      expect(()=>{
+        JLINC.verifySignature({
+          itemSigned,
+        });
+      }).to.throw('signature is required');
+
+      expect(()=>{
+        JLINC.verifySignature({
+          itemSigned,
+          signature,
+        });
+      }).to.throw('publicKey is required');
+
+      expect(()=>{
+        JLINC.verifySignature({
+          itemSigned,
+          signature,
+          publicKey,
+        });
+      }).to.throw('contextUrl is required');
+
+      expect(()=>{
+        JLINC.verifySignature({
+          itemSigned,
+          signature,
+          publicKey,
+          contextUrl,
+        });
+      }).to.not.throw();
+    });
+  });
+
+
+  it('should validate that the given signature is a signature of the give itemSigned and publicKey', function(){
+    const { itemSigned, signature, publicKey, contextUrl } = this;
 
     expect(
       JLINC.verifySignature({
         signature,
-        publicKey: rightsHolder.publicKey,
-        itemSigned: itemToSign,
-        contextUrl: JLINC.contextUrl
+        publicKey,
+        itemSigned,
+        contextUrl,
       })
     ).to.be.true;
 
     expect(() => {
       JLINC.verifySignature({
         signature: 'some fake signature i made up',
-        publicKey: rightsHolder.publicKey,
-        itemSigned: itemToSign,
-        contextUrl: JLINC.contextUrl
+        publicKey,
+        itemSigned,
+        contextUrl,
       });
     }).to.throw(JLINC.InvalidSignatureError, 'invalid signature');
 
@@ -35,17 +84,17 @@ describe('JLINC.verifySignature', function() {
       JLINC.verifySignature({
         signature,
         publicKey: JLINC.createRightsHolder().publicKey, // using the wrong public key
-        itemSigned: itemToSign,
-        contextUrl: JLINC.contextUrl
+        itemSigned,
+        contextUrl,
       });
     }).to.throw(JLINC.InvalidSignatureError, 'invalid signature');
 
     expect(()=>{
       JLINC.verifySignature({
         signature,
-        publicKey: rightsHolder.publicKey,
+        publicKey,
         itemSigned: 'this is not the item that was signed',
-        contextUrl: JLINC.contextUrl
+        contextUrl,
       });
     }).to.throw(JLINC.InvalidSignatureError, 'invalid signature');
   });
